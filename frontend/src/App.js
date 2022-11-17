8c809e9403319804e986450c834d94b1e1cfbac6
camiloATL <catorres0610@gmail.com>
2022-11-17 07:36:10 -0500

    pruebas de Producción

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import { BASE_URL } from "./config/constants";
 import Loading from "./shared/loading";
 import RoutesApp from "./routes";
+import jwt_decode from "jwt-decode";
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
@@ -13,11 +14,15 @@ function App() {
   const token = localStorage.getItem("token");
 
   useEffect(() => {
+
+    if (token){
     
     axios
       .get(BASE_URL + "/auth/verify", { headers: { Authorization: token } })
       .then(() => {
         setIsAuth(true);
+        
+        console.log(jwt_decode(token));
       })
       .catch(() => {
         setIsAuth(false);
@@ -26,6 +31,11 @@ function App() {
         setIsLoading(false)
       });
 
+    }else {
+      setIsLoading(false)
+    }
+
+
     return () => {};
   }, []);
 
@@ -33,9 +43,10 @@ function App() {
     return <Loading />;
   } else {
     return (
-      <RoutesApp isAuth={isAuth}/>
+      <RoutesApp isAuth={isAuth} setIsAuth={setIsAuth}/>
     );
   }
 }
+
 
 export default App;
--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,6 +1,10 @@
 import Swal from "sweetalert2"
+import jwt_decode from "jwt-decode";
 
-export const BASE_URL = "http://localhost:3001"
+//export const BASE_URL = "http://localhost:3001"
+export const BASE_URL = "https://omega-pet-shop-u19-team15.herokuapp.com"
+
+
 
 export const Toast = Swal.mixin({
   toast: true,
@@ -13,3 +17,5 @@ export const Toast = Swal.mixin({
     toast.addEventListener('mouseleave', Swal.resumeTimer)
   }
 })
+
+export const payload = jwt_decode(localStorage.getItem("token"))
